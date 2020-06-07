b2def9d52fa0c8447965fecf71c5bcc5891f5304
Mael <contact.squeezo1@gmail.com>
2020-06-07 16:57:15 +0200

    feat(v1.0.1): Release v1.0.1
    Fix depreacation ContainerInterface

--- src/MaelInterventionImageManager.php
+++ src/MaelInterventionImageManager.php
@@ -3,14 +3,12 @@
 namespace Mael\InterventionImageBundle;
 
 use Intervention\Image\ImageManager;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class MaelInterventionImageManager extends ImageManager
 {
 
     public function __construct(string $driver)
     {
-        //$config = $container->getParameter('mael_intervention_image.driver');
         parent::__construct([$driver]);
     }
 }
