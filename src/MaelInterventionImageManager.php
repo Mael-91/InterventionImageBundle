1b76d8af6708770715546f58372b7ff00e17d3a5
Mael <contact.squeezo1@gmail.com>
2020-06-07 16:43:43 +0200

    fix Symfony 5.1 deprecation

--- src/MaelInterventionImageManager.php
+++ src/MaelInterventionImageManager.php
@@ -8,9 +8,9 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class MaelInterventionImageManager extends ImageManager
 {
 
-    public function __construct(ContainerInterface $container)
+    public function __construct(string $driver)
     {
-        $config = $container->getParameter('mael_intervention_image.driver');
-        parent::__construct([$config]);
+        //$config = $container->getParameter('mael_intervention_image.driver');
+        parent::__construct([$driver]);
     }
 }
